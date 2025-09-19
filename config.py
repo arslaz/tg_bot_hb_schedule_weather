8d1adb377fbc3efaef1bb87304445f521c326a78
arslaz <7965118arsen@gmail.com>
2025-09-20 02:30:24 +0300

    2 commit

--- config.py
+++ config.py
@@ -1,12 +1,17 @@
-email_config = {
-    'server': 'imap.gmail.com',
-    'email': 'youraccaunt@gmail.com',
-    'password': 'yourpassword',
-    'email_sender': 'email'
-}
-bot_config = {
-    'bot_token': 'token bot',
-    'admin': ['admin id'],
-    'json_users': 'users.json',
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+email_config = {
+    'server': os.getenv('EMAIL_SERVER', 'imap.gmail.com'),
+    'email': os.getenv('EMAIL_ADDRESS'),
+    'password': os.getenv('EMAIL_PASSWORD'),
+    'email_sender': os.getenv('EMAIL_SENDER')
+}
+
+bot_config = {
+    'bot_token': os.getenv('BOT_TOKEN'),
+    'admin': os.getenv('ADMIN_IDS', '').split(','),  # Создаем список из строки
+    'json_users': os.getenv('JSON_USERS', 'users.json'),
 }
